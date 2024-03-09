e4d855b3dd2d52f239dbc3dabccf3c8d4ed8ce7e
thelordzeus <arnav55125@mail.com>
2024-03-09 18:03:44 +0530

    feat: basic setup for clerk authentication

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import { RouterProvider } from 'react-router-dom';
-import router from './routes';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { RouterProvider } from "react-router-dom";
+import router from "./routes";
+import { ConvexReactClient } from "convex/react";
+import { ClerkProvider, useAuth } from "@clerk/clerk-react";
+import { ConvexProviderWithClerk } from "convex/react-clerk";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-	<React.StrictMode>
-		<RouterProvider router={router} />
-	</React.StrictMode>
+const convex = new ConvexReactClient(import.meta.env.VITE_CONVEX_URL as string);
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <ClerkProvider publishableKey={import.meta.env.VITE_CLERK_PUBLISHABLE_KEY}>
+      <ConvexProviderWithClerk client={convex} useAuth={useAuth}>
+        <RouterProvider router={router} />
+      </ConvexProviderWithClerk>
+    </ClerkProvider>
+  </React.StrictMode>
 );
--- src/pages/static/App.tsx
+++ src/pages/static/App.tsx
@@ -1,17 +1,30 @@
-import Navbar from '../../components/layouts/Navbar';
-import Footer from '../../components/layouts/Footer';
-import Loader from '../../components/layouts/Loader';
-import { Outlet, useNavigation } from 'react-router-dom';
+import Navbar from "../../components/layouts/Navbar";
+import Footer from "../../components/layouts/Footer";
+import Loader from "../../components/layouts/Loader";
+import { Outlet, useNavigation } from "react-router-dom";
+import {
+  SignedIn,
+  SignedOut,
+  SignInButton,
+  UserButton,
+} from "@clerk/clerk-react";
 
 const App = () => {
-	const navigation = useNavigation();
-	return (
-		<div>
-			<Navbar />
-			{navigation.state === 'loading' ? <Loader /> : <Outlet />}
-			<Footer />
-		</div>
-	);
+  const navigation = useNavigation();
+  return (
+    <div>
+      <Navbar />
+      <SignedOut>
+        <SignInButton />
+      </SignedOut>
+      <SignedIn>
+        <UserButton />
+      </SignedIn>
+      {navigation.state === "loading" ? <Loader /> : <Outlet />}
+
+      <Footer />
+    </div>
+  );
 };
 
 export default App;
