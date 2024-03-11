d125f885dc9656a46863629855de8c3852e7bf39
kunalkeshan <kunalkeshan12@gmail.com>
2024-03-11 17:06:31 +0530

    fix: build issues

--- src/components/ContributionsGraph.tsx
+++ src/components/ContributionsGraph.tsx
@@ -1,36 +1,39 @@
-import { useState } from "react";
+import { useState } from 'react';
 
 const ContributionsGraph = () => {
-  // State to store recorded days
-  const [recordedDays, setRecordedDays] = useState([]);
+	// State to store recorded days
+	const [recordedDays] = useState<string[]>([]);
 
-  // Logic to generate the array of day grids
-  const generateDayGrids = () => {
-    const dayGrids = [];
-    for (let i = 0; i < 200; i++) {
-      const currentDate = new Date();
-      currentDate.setDate(currentDate.getDate() - i);
-      const formattedDate = currentDate.toISOString().slice(0, 10);
+	// Logic to generate the array of day grids
+	const generateDayGrids = () => {
+		const dayGrids = [];
+		for (let i = 0; i < 200; i++) {
+			const currentDate = new Date();
+			currentDate.setDate(currentDate.getDate() - i);
+			const formattedDate = currentDate.toISOString().slice(0, 10);
 
-      // Check if the formatted date is in the recordedDays array
-      const isActive = recordedDays.includes(formattedDate);
+			// Check if the formatted date is in the recordedDays array
+			const isActive = recordedDays.includes(formattedDate);
 
-      // Add div to dayGrids array with appropriate class
-      dayGrids.unshift(
-        <div key={i} className={`day ${isActive ? "day--active" : ""}`}></div>
-      );
-    }
-    return dayGrids;
-  };
+			// Add div to dayGrids array with appropriate class
+			dayGrids.unshift(
+				<div
+					key={i}
+					className={`day ${isActive ? 'day--active' : ''}`}
+				></div>
+			);
+		}
+		return dayGrids;
+	};
 
-  // Render the component
-  return (
-    <div className="border border-white rounded-xl p-6 mb-3">
-      <h2 className="text-md font-semibold">Book Reading</h2>
-      <p className="text-sm">Read 5 pages of Harry Potter daily</p>
-      <div className="days mt-3">{generateDayGrids()}</div>
-    </div>
-  );
+	// Render the component
+	return (
+		<div className='border border-white rounded-xl p-6 mb-3'>
+			<h2 className='text-md font-semibold'>Book Reading</h2>
+			<p className='text-sm'>Read 5 pages of Harry Potter daily</p>
+			<div className='days mt-3'>{generateDayGrids()}</div>
+		</div>
+	);
 };
 
 export default ContributionsGraph;
--- src/components/landing/OnePercentStats.tsx
+++ src/components/landing/OnePercentStats.tsx
@@ -1,27 +1,20 @@
 import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
+	Card,
+	CardDescription,
+	CardHeader,
+	CardTitle,
+} from '@/components/ui/card';
 
 export default function OnePercentStats() {
-  return (
-    <Card className="mb-4">
-      <CardHeader>
-        <CardTitle>1% Daily Graph</CardTitle>
-        <CardDescription>
-          See how much you have progressed this year with the 1% Theory
-        </CardDescription>
-      </CardHeader>
-      {/* <CardContent>
-        <p>Card Content</p>
-      </CardContent>
-      <CardFooter>
-        <p>Card Footer</p>
-      </CardFooter> */}
-    </Card>
-  );
+	return (
+		<Card className='mb-4'>
+			<CardHeader>
+				<CardTitle>1% Daily Graph</CardTitle>
+				<CardDescription>
+					See how much you have progressed this year with the 1%
+					Theory
+				</CardDescription>
+			</CardHeader>
+		</Card>
+	);
 }
