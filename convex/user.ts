c5b2bef7987db1543f1099b989426104f490fc0a
kunalkeshan <kunalkeshan12@gmail.com>
2024-03-12 00:45:59 +0530

    chore: efactor storeUser function in user.ts

--- convex/user.ts
+++ convex/user.ts
@@ -1,30 +1,34 @@
-import { ConvexError } from "convex/values";
-import { mutation } from "./_generated/server";
+import { ConvexError } from 'convex/values';
+import { mutation } from './_generated/server';
 
 export const store = mutation({
-  args: {},
-  handler: async (ctx, args) => {
-    const identity = await ctx.auth.getUserIdentity();
-    if (!identity) {
-      throw new ConvexError("Called storeUser without authentication present");
-    }
+	args: {},
+	handler: async (ctx) => {
+		const identity = await ctx.auth.getUserIdentity();
+		if (!identity) {
+			throw new ConvexError(
+				'Called storeUser without authentication present'
+			);
+		}
 
-    // Check if we've already stored this identity before.
-    const user = await ctx.db
-      .query("users")
-      .filter((q) => q.eq(q.field("tokenIdentifier"), identity.tokenIdentifier))
-      .unique();
-    if (user !== null) {
-      // If we've seen this identity before but the name has changed, patch the value.
-      if (user.name !== identity.name) {
-        await ctx.db.patch(user._id, { name: identity.name });
-      }
-      return user._id;
-    }
-    // If it's a new identity, create a new `User`.
-    return await ctx.db.insert("users", {
-      name: identity.name!,
-      tokenIdentifier: identity.tokenIdentifier,
-    });
-  },
+		// Check if we've already stored this identity before.
+		const user = await ctx.db
+			.query('users')
+			.filter((q) =>
+				q.eq(q.field('tokenIdentifier'), identity.tokenIdentifier)
+			)
+			.unique();
+		if (user !== null) {
+			// If we've seen this identity before but the name has changed, patch the value.
+			if (user.name !== identity.name) {
+				await ctx.db.patch(user._id, { name: identity.name });
+			}
+			return user._id;
+		}
+		// If it's a new identity, create a new `User`.
+		return await ctx.db.insert('users', {
+			name: identity.name!,
+			tokenIdentifier: identity.tokenIdentifier,
+		});
+	},
 });
