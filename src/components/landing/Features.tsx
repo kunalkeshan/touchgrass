962a8ac107b93b79c6c01c4d66ab38deff66d04b
Kunal Keshan <kunalkeshan12@gmail.com>
2024-03-22 06:59:21 +0530

    chore: user migration updates (wip)

--- src/components/landing/Features.tsx
+++ src/components/landing/Features.tsx
@@ -114,45 +114,45 @@ const Features = () => {
 				<h2 className='text-xl lg:text-3xl text-center font-semibold'>
 					Why Touchgrass?
 				</h2>
+				<ul className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mt-8'>
+					{features.map((feature, idx) => (
+						<li
+							key={`feature-${idx}`}
+							className='inline-flex p-2 border rounded-lg relative gap-2 hover:-translate-y-1 select-none hover:shadow-sm transition-all duration-300 hover:shadow-white'
+						>
+							<feature.Icon className='shrink-0 mt-1' />
+							<div>
+								<h3 className='text-base lg:text-lg font-medium'>
+									{feature.title}
+								</h3>
+								<p className='text-sm text-slate-300'>
+									{feature.description}
+								</p>
+							</div>
+							<Tooltip>
+								<TooltipTrigger
+									className='absolute top-0 right-0 transform translate-x-1/2 -translate-y-1/2'
+									asChild
+								>
+									{feature.ready ? (
+										<CircleCheck
+											className='text-green-500'
+											fill='white'
+										/>
+									) : (
+										<TrafficCone className='text-orange-500' />
+									)}
+								</TooltipTrigger>
+								<TooltipContent>
+									{feature.ready
+										? 'This feature is ready to use.'
+										: 'This feature is coming soon!'}
+								</TooltipContent>
+							</Tooltip>
+						</li>
+					))}
+				</ul>
 			</div>
-			<ul className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mt-8'>
-				{features.map((feature, idx) => (
-					<li
-						key={`feature-${idx}`}
-						className='inline-flex p-2 border rounded-lg relative gap-2 hover:-translate-y-1 select-none hover:shadow-sm transition-all duration-300 hover:shadow-white'
-					>
-						<feature.Icon className='shrink-0 mt-1' />
-						<div>
-							<h3 className='text-base lg:text-lg font-medium'>
-								{feature.title}
-							</h3>
-							<p className='text-sm text-slate-300'>
-								{feature.description}
-							</p>
-						</div>
-						<Tooltip>
-							<TooltipTrigger
-								className='absolute top-0 right-0 transform translate-x-1/2 -translate-y-1/2'
-								asChild
-							>
-								{feature.ready ? (
-									<CircleCheck
-										className='text-green-500'
-										fill='white'
-									/>
-								) : (
-									<TrafficCone className='text-orange-500' />
-								)}
-							</TooltipTrigger>
-							<TooltipContent>
-								{feature.ready
-									? 'This feature is ready to use.'
-									: 'This feature is coming soon!'}
-							</TooltipContent>
-						</Tooltip>
-					</li>
-				))}
-			</ul>
 		</section>
 	);
 };
--- src/components/landing/Hero.tsx
+++ src/components/landing/Hero.tsx
@@ -2,6 +2,8 @@ import { SignInButton } from '@clerk/clerk-react';
 import { Authenticated, Unauthenticated, AuthLoading } from 'convex/react';
 import { Button } from '../ui/button';
 import { Link } from 'react-router-dom';
+import { Alert, AlertDescription, AlertTitle } from '@/components/ui/alert';
+import { Rocket } from 'lucide-react';
 
 const Hero = () => {
 	const people = [
@@ -30,51 +32,78 @@ const Hero = () => {
 	return (
 		<section className='p-4 md:p-16 bg-[url(/images/bg-lines.png)] min-h-[60vh] w-full'>
 			<div className='w-full max-w-5xl 2xl:max-w-7xl mx-auto'>
-				<h1 className='text-2xl lg:text-4xl text-center font-bold'>
+				<Alert className='bg-transparent text-white max-w-3xl mx-auto bg-zinc-900 lowercase shadow-white shadow-sm hover:shadow-md hover:shadow-white transition-all duration-300 hover:-translate-y-1'>
+					<Rocket
+						className='h-4 w-4 !text-white shrink-0'
+						size={40}
+						strokeWidth={2}
+					/>
+					<AlertTitle className='text-lg lg:text-xl'>
+						<span className='text-green-500'>touchgrass </span>
+						update : Temporary{' '}
+						<span className='text-red-500'>
+							Unavailability
+						</span>{' '}
+						during Migration
+					</AlertTitle>
+					<AlertDescription className='font-normal'>
+						Dear <u>touchgrass Users</u>, we are excited to announce
+						that our project is transitioning into a{' '}
+						<u>full-fledged product!</u> As we <u>migrate</u> the
+						app to enhance your experience, Touch Grass will be{' '}
+						<u>temporarily unavailable</u>. Stay tuned for updates
+						on our <u>product timeline</u>. We{' '}
+						<u>apologize for any inconvenience</u> and appreciate
+						your understanding as we work towards bringing you an
+						even better platform for your personal growth journey.{' '}
+						<u>Thank you for your patience!</u>
+					</AlertDescription>
+				</Alert>
+				<h1 className='text-2xl lg:text-4xl text-center font-bold mt-8'>
 					Welcome to Touchgrass. Your habit tracking app.
 				</h1>
-			</div>
-			<div>
-				<div className='grid grid-cols-2 gap-4 md:grid-cols-3 lg:grid-cols-5 mt-8 place-items-center'>
-					{people.map((person, index) => (
-						<div
-							key={`person-${index}`}
-							className='max-w-32 text-center'
-						>
-							<img
-								src={person.image}
-								alt={person.text}
-								className='w-full h-auto object-cover'
-							/>
-							<p className='mt-2'>{person.text}</p>
-						</div>
-					))}
+				<div>
+					<div className='grid grid-cols-2 gap-4 md:grid-cols-3 lg:grid-cols-5 mt-8 place-items-center'>
+						{people.map((person, index) => (
+							<div
+								key={`person-${index}`}
+								className='max-w-32 text-center'
+							>
+								<img
+									src={person.image}
+									alt={person.text}
+									className='w-full h-auto object-cover'
+								/>
+								<p className='mt-2'>{person.text}</p>
+							</div>
+						))}
+					</div>
 				</div>
-			</div>
-			<div className='mt-8 flex gap-4 justify-center'>
-				<Authenticated>
-					<Button className='' variant={'secondary'} asChild>
-						<Link to={'/app'}>App</Link>
+				<div className='mt-8 flex gap-4 justify-center'>
+					<Authenticated>
+						<Button className='' variant={'secondary'} asChild>
+							<Link to={'/app'}>App</Link>
+						</Button>
+					</Authenticated>
+					<Unauthenticated>
+						<Button className='' variant={'secondary'} asChild>
+							<SignInButton
+								afterSignInUrl='/app'
+								afterSignUpUrl='/app'
+							>
+								Get Started
+							</SignInButton>
+						</Button>
+					</Unauthenticated>
+					<AuthLoading>
+						<Button className='' variant={'secondary'} disabled>
+							Loading
+						</Button>
+					</AuthLoading>
+					<Button asChild className='bg-black hover:bg-black/80'>
+						<Link to={'/#features'}>View features</Link>
 					</Button>
-				</Authenticated>
-				<Unauthenticated>
-					<Button className='' variant={'secondary'} asChild>
-						<SignInButton
-							afterSignInUrl='/app'
-							afterSignUpUrl='/app'
-						>
-							Get Started
-						</SignInButton>
-					</Button>
-				</Unauthenticated>
-				<AuthLoading>
-					<Button className='' variant={'secondary'} disabled>
-						Loading
-					</Button>
-				</AuthLoading>
-				<Button asChild className='bg-black hover:bg-black/80'>
-					<Link to={'/#features'}>View features</Link>
-				</Button>
+				</div>
 			</div>
 		</section>
 	);
